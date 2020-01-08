d864477c41d6aeaaf65e86cb7fe7c5ee352af1af
adafs <164092858@qq.com>
2020-01-08 23:27:17 +0800

    init

---
+++ app/src/main/java/com/adafs/libflavor/MainActivity.java
@@ -0,0 +1,17 @@
+package com.adafs.libflavor;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView textView = findViewById(R.id.text);
+        textView.setText(R.string.flavor);
+    }
+}
